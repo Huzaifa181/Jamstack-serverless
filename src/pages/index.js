ad793b503cada62af80059c1ef41db86bd5f5ba9
Huzaifa Ahmed <huzaifa.ahmed181@gmail.com>
2021-09-29 04:25:44 +0500

    Gatsby Starter

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import * as React from "react"
+import { Link } from "gatsby"
+const Home = () => {
+  return (
+    <div>Hello World
+      <Link to="/about">About</Link>
+    </div>
+  )
+}
+
+export default Home
