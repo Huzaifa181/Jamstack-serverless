d6aebe8b56399fa7d54bb7576c68dca1edb1c104
Huzaifa Ahmed <huzaifa.ahmed181@gmail.com>
2021-09-29 04:34:12 +0500

    Linking

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,12 @@
 import * as React from "react"
-import { Link } from "gatsby"
+import { Link, navigate } from "gatsby"
 const Home = () => {
   return (
     <div>Hello World
       <Link to="/about">About</Link>
+      <button onClick={()=>{
+        navigate('/about')
+      }}>About page with button</button>
     </div>
   )
 }
